4e11cdf8ed8df36c7f996d4482b507ec6688d711
Ziko <Ziko@gmail.com>
2020-04-12 00:58:30 +0100

    Update script.js

--- C3_Training_DOM_Montre-Analogue-master/script.js
+++ C3_Training_DOM_Montre-Analogue-master/script.js
@@ -4,22 +4,18 @@ const AIGUILLEMIN = document.querySelector("#minute");
 const AIGUILLESEC = document.querySelector("#second");
 
 //Extraire l'heure actuel à l'aide de l'objet Date()
-const date = new Date();
+
 console.log();
 
 //Ajouter l'heure , minite , seconde  dans des varaiables
-const haours = date.getHours();
-const minute = date.getMinutes();
-const seconde = date.getSeconds();
+
 // console.log(haours);
 // console.log(minute);
 // console.log(seconde);
 
 // Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
 // Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
-const dugreHaours = haours * 30;
-const dugreMinute = minute * 6;
-const dugreSeconde = seconde * 6;
+
 
 
 
@@ -28,10 +24,16 @@ const dugreSeconde = seconde * 6;
 // console.log(dugreSeconde);
 // Déplacer les aiguilles 
 function demarrerLaMontre() {
+    const date = new Date();
+    const haours = date.getHours();
+    const minute = date.getMinutes();
+    const seconde = date.getSeconds();
+    const dugreSeconde = seconde * 6;
+    const dugreMinute = minute  * 6;
+    const dugreHaours = haours * 30;
     AIGUILLEHR.style.transform = `rotate(${dugreHaours}deg)`;
     AIGUILLEMIN.style.transform = `rotate(${dugreMinute}deg)`;
     AIGUILLESEC.style.transform = `rotate(${dugreSeconde}deg)`;
-    
 
 }
 // Exercuter la fonction chaque second
