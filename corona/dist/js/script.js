51ac14809bbaaf868d78ec4c032dc24cea9b3e6d
Ziko <Ziko@gmail.com>
2020-05-09 15:24:05 +0000

    creat function start test

--- corona/dist/js/script.js
+++ corona/dist/js/script.js
@@ -1,8 +1,16 @@
-const start = document.querySelector("start");
-
+const start = document.querySelector(".start");
+const stepers = document.querySelectorAll(".stepers");
+const preambule = document.querySelector(".Preambule");
+const contentQuestion = document.querySelector(".test");
+// console.log(stepers);
+// console.log(stepers[0]);
 
 start.addEventListener("click", firstStep);
 
 function firstStep(){
-
+    start.style.display = "none";
+    stepers[0].classList.remove("shadow");
+    stepers[1].classList.add("shadow");
+    preambule.style.display = "none";
+    contentQuestion.classList.remove("remove");
 }
